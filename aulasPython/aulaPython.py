9f87369cd75e5a4a35b52ed0bd7ddb5179b9379c
Leonardo Farias <d3pro3@outlook.com>
2022-07-03 16:10:13 -0300

    Inicio aulas em python

---
+++ aulasPython/aulaPython.py
@@ -0,0 +1,35 @@
+print("Olá mundo")
+
+
+ # comentario de uma linha
+ 
+"""
+comentario de multiplas linhas
+comentario de multiplas linhas
+comentario de multiplas linhas
+"""
+
+# COMANDO PARA PYTHON RECONHECER ACENTOS TIL ETC
+# -*- coding: utf-8 -*- (parece estar obsoleto, revisar depois.!)
+
+print("Olá mundo");
+
+                     # OPERADORES MATEMÁTICOS
+
+# EXPONENCIAÇÃO
+"""
+utiliza-se **
+Exemplo 2 elevado a 2.
+"""
+print( 2 ** 3); # dois elevado a 3 = a 8 (2.2.2)
+
+# MODULO
+"""
+utiliza-se %
+Modulo é o resto da divisão
+Exemplo 10 dividido por 3 da  3 e resto 1.
+"""
+print (10 % 3); # 10 dividido por 3 resto é 1.
+
+                    # OPERADORES LOGICOS EM PYTHON
+
